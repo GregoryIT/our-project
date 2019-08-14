69f9548655d9ec37620e7e3b15c0b3b9dcd2b1a1
GregoryIT <lenovibe.c2@gmail.com>
2019-08-14 18:30:32 +0300

    added anchor links with smooth scrolling

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    $(".top-bar").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 700);
+    });
+});
+
+$(document).ready(function () {
     $("#phone").mask("+ 375 (--) -- -- ---");
 });
 
